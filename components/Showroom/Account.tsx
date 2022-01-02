41ec40e89a86ca829c3166496fb7871e6db4c1a8
Cristi Cretu <crisemcr@gmail.com>
2022-01-02 18:27:05 +0200

    Semi-finished component

--- components/Showroom/Account.tsx
+++ components/Showroom/Account.tsx
@@ -9,16 +9,20 @@ interface AccountProps {
 
 const Account: React.FC<AccountProps> = ({ username, pfp, description }) => {
   return (
-    <div className='rounded-lg px-6 py-8 flex flex-col space-y-4 items-center bg-gray-200 dark:bg-gray-900'>
-      <Image
-        src={pfp}
-        alt='NFT'
-        width={100}
-        height={100}
-        className='rounded-full -mt-4'
-      />
-      <h1>@{username}</h1>
-      <p className='truncate'>{description}</p>
+    <div className='rounded-lg px-3 py-6 flex items-center space-x-4  bg-gray-200 dark:bg-gray-900'>
+      {pfp && (
+        <Image
+          src={pfp}
+          alt='Profile Picture'
+          width={100}
+          height={100}
+          className='rounded-full'
+        />
+      )}
+      <div>
+        <h1 className='font-bold text-lg grad'>@{username}</h1>
+        <p className='truncate font-semibold'>{description}</p>
+      </div>
     </div>
   );
 };
